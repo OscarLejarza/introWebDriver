2a47d85bb1035386d5b9b4c1eda40570573dafa7
Oscar Lejarza <oscar.lejarza@astrazeneca.com>
2020-06-28 20:10:14 -0500

    add

--- src/intro/TelcelSeleccionarArticulo.java
+++ src/intro/TelcelSeleccionarArticulo.java
@@ -1,7 +1,10 @@
 package intro;
 
 import classExercise.Celular;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TelcelSeleccionarArticulo {
 
@@ -14,6 +17,59 @@ public class TelcelSeleccionarArticulo {
         verificarPaginaResultados();
         Celular primerCelular = capturarDatosCelular(1);
         seleccionarCelular(1);
-        
+        validarDatosCelular(primerCelular);
+
     }
+
+    private static void navegarSitio(String url) {
+        driver = new ChromeDriver();
+
+        driver.navigate().to(url);
+    }
+
+    private static void verificarLandingPage() {
+        // verificar que existen estos elementos
+        // logoTelcel:
+        WebElement logoTelcel = driver.findElement(By.cssSelector("[src='/content/dam/htmls/img/icons/logo-telcel.svg']"));
+        WebElement tiendaEnLinea = driver.findElement(By.cssSelector("[data-nombreboton='Tienda en linea superior']"));
+        WebElement campoBusqueda = driver.findElement(By.cssSelector("#buscador-menu-input"));
+        if (logoTelcel.isDisplayed() &&
+                tiendaEnLinea.isDisplayed() &&
+                campoBusqueda.isDisplayed() && campoBusqueda.isEnabled()){
+ //       if (driver.findElement(By.cssSelector("[src='/content/dam/htmls/img/icons/logo-telcel.svg']")).isDisplayed() &&
+ //               driver.findElement(By.cssSelector("[data-nombreboton='Tienda en linea superior']")).isDisplayed() &&
+ //               driver.findElement(By.cssSelector("#buscador-menu-input")).isDisplayed()) {
+            System.out.println("Si cargo la pagina principal de telcel");
+        } else {
+            System.out.println("No cargo la pagina");
+            System.exit(-1); // Para indicar que es un error, si aparece 0 es que no hay error
+        }
+        // linkTiendaEnLinea
+
+        // campoBusqueda
+
+
+
+
+    }
+
+    private static void listarTelefonos() {
+    }
+
+    private static void verificarPaginaResultados() {
+
+    }
+
+    private static Celular capturarDatosCelular(int i) {
+        return null;
+    }
+
+    private static void seleccionarCelular(int numCelular) {
+
+    }
+
+    private static void validarDatosCelular(Celular primerCelular){
+
+    }
+
 }
