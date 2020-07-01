98abfddd9af66389d381f8c917b1a13ecdd99012
Oscar Lejarza <oscar.lejarza@astrazeneca.com>
2020-07-01 15:47:13 -0500

    songsbysinatra

---
+++ src/tareas/SongsSinatraParent.java
@@ -0,0 +1,23 @@
+package tareas;
+
+import org.openqa.selenium.WebDriver;
+
+public class SongsSinatraParent {
+
+    public static WebDriver driver;
+
+    public static void navegar(String url) {
+    }
+
+    public static void realizarLoginCorrecto(String usuario, String password) {
+    }
+
+    public static void validarHomePage() {
+    }
+
+    public static void validarMensajeBienvenida(String mensajeBienvenida) {
+    }
+
+    public static void cerrarBrowser() {
+    }
+}
